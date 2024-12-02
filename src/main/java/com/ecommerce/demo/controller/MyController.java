7918a75ad08e5f1b9f1273c0137a960017ff6229
Priyaranjan Sahoo <priyaranjansahoo039@gmail.com>
2024-12-02 20:41:24 +0530

    1st commit

---
+++ src/main/java/com/ecommerce/demo/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.ecommerce.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String webView() {
+		return "login";
+	}
+	
+	 
+	
+}
