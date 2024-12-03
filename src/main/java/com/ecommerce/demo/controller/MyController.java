ae239d7ae485a313a2631a22dece13f9ee54f45c
Priyaranjan Sahoo <priyaranjansahoo039@gmail.com>
2024-12-04 00:36:01 +0530

    1st

---
+++ src/main/java/com/ecommerce/demo/controller/MyController.java
@@ -0,0 +1,29 @@
+package com.ecommerce.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String webView() {
+		return "login";
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	@GetMapping("/register")
+	public String Register() {
+		return "register";
+	}
+	
+	 @GetMapping("/newpage")
+	    public String newPage() {
+	        return "newpage"; 
+	    }
+	
+	
+	
+}
