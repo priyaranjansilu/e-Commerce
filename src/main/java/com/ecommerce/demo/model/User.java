6450690a5868311e9a3b4e8b166dd36d696dbd7b
Priyaranjan Sahoo <priyaranjansahoo039@gmail.com>
2024-12-06 18:54:46 +0530

    some changes added(Project Completed)

--- src/main/java/com/ecommerce/demo/model/User.java
+++ src/main/java/com/ecommerce/demo/model/User.java
@@ -1,6 +1,17 @@
 package com.ecommerce.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
     private String username;
     private String email;
     private String password;
